bdbb68187cc40bb8384a61706a30f70b67fb1464
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-06 19:59:22 +0300

    Core Basics

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+        int i = 0;
+
+        for (i = 97; i < 123; i++){
+        putchar(i);
+        }
+        putchar('\n');
+
+        return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+        int i = 0;
+
+        for (i = 97; i < 123; i++){
+        putchar(i);
+        }
+        for (i = 65; i < 91; i++){
+        putchar(i);
+        }
+        putchar('\n');
+
+        return (0);
+}
